fac65c643db699612ed483778ea94516bdd6c8b4
Bartosz "Simek" Kaszubowski <gosimek@gmail.com>
2013-04-26 08:30:42 +0200

    sidebar init + CSS fixes

--- xpather.js
+++ xpather.js
@@ -1,31 +1,51 @@
+var xpatherHTML = '\
+	<div id="xpather">\
+		<form id="xpather-form">\
+			<input id="xpather-xpath" type="text" />\
+		</form> \
+		<div id="xpather-result"></div>\
+		<div id="xpather-sidebar-toggler"></div>\
+		</div>\
+	<div id="xpather-sidebar"></div>'
+
 var previousMatched = [];
 
 var body = $('body');
-body.append($('<div id="xpather"><form id="xpather-form"><input id="xpather-xpath" type="text" /></form><div id="xpather-result"></div></div>').hide());
+body.append(xpatherHTML);
 
 var xpather = $('#xpather');
 var resultBox = $('#xpather-result');
 var xpathInput = $('#xpather-xpath');
 var xpathForm = $('#xpather-form');
+var sidebar = $('#xpather-sidebar');
+var sidebarToggler = $('#xpather-sidebar-toggler');
 
 function init() {
+	body.toggleClass('xpather-on');
 	if (xpather.is(":visible") == false) {
-		body.css('margin-top', '50px');
 		xpather.show();
 		xpathInput.focus();
 		xpathForm.bind('submit', function () {
 			find(xpathInput.val());
 			return false;
 		});
+		sidebarToggler.bind('click', function () {
+			body.toggleClass('xpather-sidebar-on');
+			sidebarToggler.toggleClass('xpather-sidebar-toggler-active');
+			sidebar.toggle();
+			return false;
+		});
 	} else {
-		body.css('margin-top', '0');
 		xpather.hide();
+		sidebar.hide();
+		body.removeClass('xpather-sidebar-on');
 		clearHighlight(previousMatched);
 	}
 }
 
 function find(xpath) {
 	if (previousMatched.length != 0) {
+		sidebar.empty();
 		clearHighlight(previousMatched);
 	}
 
@@ -36,7 +56,13 @@ function find(xpath) {
 		previousMatched = result;
 		if (result.length != 0) {
 			$.each(result, function (key, value) {
-				$(value).addClass('xpather-highlight');
+				var node = $(value);
+				node.addClass('xpather-highlight');
+				if(node.text().length != 0) {
+					sidebar.append($('<div class="xpather-sidebar-entry" />').text(getNodeText(node)));
+				} else {
+					sidebar.append($('<div class="xpather-sidebar-entry xpather-sidebar-entry-empty" />').text("EMPTY NODE"));
+				}
 			});
 			resultBox.removeClass('no-results').text(result.length);
 			console.log(result);
@@ -52,4 +78,8 @@ function clearHighlight(nodes) {
 		$(value).removeClass('xpather-highlight');
 	});
 	$('*[class=""]').removeAttr('class');
+}
+
+function getNodeText(node) {
+	return $.trim(node.text().replace(/ +(?= )/g,' '));
 }
